3713a5cbabd5dc1ef4ee1649515df4b15eb78bb1
SOL <1846326091@qq.com>
2016-09-22 10:41:04 +0800

    Initialize!

---
+++ compiler/scanner/scanner.h
@@ -0,0 +1,22 @@
+#include "../../platform.h" 
+
+namespace ACPL
+{
+    class Scanner
+    {
+    private:
+        // File input stream
+        Tifstream fin;
+
+    public:
+
+        // [Interface]
+        // Open the specified file
+        void open(Tstring filename);
+
+        // [Interface]
+        // Read the next token
+        // Returns the ID of the token received 
+        Token read();
+    }
+}
